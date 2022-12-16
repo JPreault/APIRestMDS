3d56ae17128c8b273da6bccdf8627d5905524e96
José <jose@preault.com>
2022-12-16 23:59:25 +0100

    feat(back-end):Ajout des noms pour la liste globale

--- src/Service/QueryService.php
+++ src/Service/QueryService.php
@@ -36,19 +36,14 @@ class QueryService
 
     public function getAllProducts(): array 
     {
-        /*$codeIsExistInOFF = file_get_contents('https://world.openfoodfacts.org/api/v2/search?code='.$productDetails['code']);
-            if($codeIsExistInOFF['count'] = 0){
-                $message = 'Error encountered : This code isn\'t assigned to a reel product';
-                $product = ['id' => $message, 'code'=> $message,'stock' => 0,'name' => $message];
-            } else {
-                $product = new Product();
-                $product->setName($productDetails['name']);
-                $product->setStock($productDetails['stock']);
-                $product->setCode($productDetails['code']);
-
-                $this->manager->persist($product);
-                $this->manager->flush();
-            }*/
-        return $this->productRepository->findAll();
+        $productList = $this->productRepository->findAll();
+        foreach ($productList as $key => $product) {
+            if($product->getName() === ''){
+                $productFromApiOFF = json_decode(file_get_contents('https://world.openfoodfacts.org/api/v2/search?code='.$product->getCode().'&fields=product_name'),true);
+                $product->setName($productFromApiOFF['products'][0]['product_name']);
+                $productList[$key] = $product;
+            }
+        }
+        return $productList;
     }
 }
